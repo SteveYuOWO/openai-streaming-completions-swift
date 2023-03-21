be21622e56d3942946c97e1c6bd8d7ec392097a6
Steve Yu <littlepageprogram@163.com>
2023-03-22 04:30:02 +0800

    feat: support proxy for openai (#2)

--- Sources/OpenAIStreamingCompletions/OpenAI.swift
+++ Sources/OpenAIStreamingCompletions/OpenAI.swift
@@ -3,10 +3,18 @@ import Foundation
 public struct OpenAIAPI {
     var apiKey: String
     var orgId: String?
+    var origin = "https://api.openai.com"
 
-    public init(apiKey: String, orgId: String? = nil) {
+    public init(apiKey: String, orgId: String? = nil, origin: String? = nil) {
         self.apiKey = apiKey
         self.orgId = orgId
+        if let origin = origin {
+            if origin.hasSuffix("/") {
+                self.origin = String(origin.dropLast())
+            } else {
+                self.origin = origin
+            }
+        }
     }
 }
 
